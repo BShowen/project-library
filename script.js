8c2e1896ac364ff126059b726fd6244cd577be11
Bradley Showen <bshowen@live.seminolestate.edu>
2021-12-17 06:50:18 -0500

    form submission is working

--- script.js
+++ script.js
@@ -62,56 +62,28 @@ function BookElement(params){
   return div;
 }
 
-// This is test code. Delete when finished with it. 
-let testBook_1 = new Book({title: "Book 1", author: "Book 1 Author", pages: 300, read: true});
-let testBook_2 = new Book({title: "Book 2", author: "Book 2 Author", pages: 1002, read: false});
-let testBook_3 = new Book({title: "Book 3", author: "Book 2 Author", pages: 1002, read: false});
-let testBook_4 = new Book({title: "Book 4", author: "Book 2 Author", pages: 1002, read: false});
-let testBook_5 = new Book({title: "Book 5", author: "Book 2 Author", pages: 1002, read: false});
-let testBook_6 = new Book({title: "Book 6", author: "Book 2 Author", pages: 1002, read: false});
-let testBook_7 = new Book({title: "Book 7", author: "Book 2 Author", pages: 1002, read: false});
-let testBook_8 = new Book({title: "Book 8", author: "Book 2 Author", pages: 1002, read: false});
-let testBook_9 = new Book({title: "Book 9", author: "Book 2 Author", pages: 1002, read: false});
-let testBook_10 = new Book({title: "Book 10", author: "Book 2 Author", pages: 1002, read: false});
-let testBook_11 = new Book({title: "Book 11", author: "Book 2 Author", pages: 1002, read: false});
-let testBook_12 = new Book({title: "Book 12", author: "Book 2 Author", pages: 1002, read: false});
-addBookToLibrary(testBook_1);
-addBookToLibrary(testBook_2);
-addBookToLibrary(testBook_3);
-addBookToLibrary(testBook_4);
-addBookToLibrary(testBook_5);
-addBookToLibrary(testBook_6);
-addBookToLibrary(testBook_7);
-addBookToLibrary(testBook_8);
-addBookToLibrary(testBook_9);
-addBookToLibrary(testBook_10);
-addBookToLibrary(testBook_11);
-addBookToLibrary(testBook_12);
-
 // Here, we are adding the books to the DOM. 
-const libraryElement = document.querySelector("#library");
-library.forEach( book => {
-  const card = new Card();
-  const currentBook = new BookElement(book);
-  card.appendChild(currentBook);
-  libraryElement.appendChild(card);
-});
-
-
+function updateDOM(){
+  const libraryElement = document.querySelector("#library");
+  libraryElement.innerHTML = "";
+  library.forEach( (book, index) => {
+    const card = new Card();
+    // Add the index to the card. This is used for identification throughout the lifecycle of the card. 
+    card.dataset.index = index;
+    const currentBook = new BookElement(book);
+    card.appendChild(currentBook);
+    libraryElement.appendChild(card);
+  });
+}
 
 // Nav button
 const navButton = document.querySelector("#nav > button");
 navButton.addEventListener("click", ()=>{
   showModalForm();
 });
-navButton.click();
 
 // Show the modal form. 
 function showModalForm(){
-  // Prevent scrolling while modal is open. 
-  const html = document.querySelector("html");
-  html.classList.toggle("fixed");
-
   // Show the model. 
   const modalWindow = document.querySelector(".modal");
   modalWindow.classList.toggle("hidden");
@@ -127,14 +99,8 @@ function showModalForm(){
 // Let the user close the modal by hitting "escape";
 (function escapeKey(){
   document.addEventListener("keydown", (e)=>{
-
     const modalWindow = document.querySelector(".modal");
-    
-    if(e.code.toLowerCase() === "escape" && !modalWindow.classList.contains("hidden")){
-      // Allow the body of the page to scroll again. 
-      const html = document.querySelector("html");
-      html.classList.toggle("fixed");
-  
+    if(e.code && e.code.toLowerCase() === "escape" && !modalWindow.classList.contains("hidden")){
       // Hide the modal. 
       modalWindow.classList.toggle("hidden");
       modalWindow.style.backgroundColor = "rgba(0,0,0,0.00)";
@@ -147,14 +113,9 @@ function showModalForm(){
   const modal = document.querySelector(".modal");
   modal.addEventListener("click", (e)=>{
     if(e.target.classList.contains("modal")){ 
-      const modalWindow = document.querySelector(".modal");
-      // Allow the body of the page to scroll again. 
-      const html = document.querySelector("html");
-      html.classList.toggle("fixed");
-  
       // Hide the modal. 
-      modalWindow.classList.toggle("hidden");
-      modalWindow.style.backgroundColor = "rgba(0,0,0,0.00)";
+      modal.classList.toggle("hidden");
+      modal.style.backgroundColor = "rgba(0,0,0,0.00)";
     }
   });
 })();
@@ -163,4 +124,38 @@ function showModalForm(){
 const formSubmitButton = document.querySelector("#formSubmitButton");
 formSubmitButton.addEventListener("click", (e) => {
   e.preventDefault();
+  const formData = getFormData();
+  if( formData.valid ){
+    addBookToLibrary(formData.book);
+    // Hide the modal. 
+    const modal = document.querySelector(".modal");
+    modal.classList.toggle("hidden");
+    modal.style.backgroundColor = "rgba(0,0,0,0.00)";  
+    updateDOM();
+  }else{
+    console.log("INVALID");
+  }
 });
+
+function getFormData(){
+  const formData = {valid: false};
+  const title = document.querySelector("#bookTitle");
+  const author = document.querySelector("#bookAuthor");
+  const pages = document.querySelector("#bookPages");
+  const readStatus = document.querySelector("input[name=readStatus]:checked");
+  
+  formData.valid = !!title.value && !!author.value && !!pages.value && !!readStatus.value;
+  
+  if(formData.valid){
+    formData.book = new Book({
+      title: title.value, 
+      author: author.value, 
+      pages: pages.value, 
+      read: readStatus.value,
+    });
+  }
+
+  return formData;
+}
+
+updateDOM();
